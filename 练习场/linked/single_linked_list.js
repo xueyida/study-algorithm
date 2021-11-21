24d4f9ace01fbfa33121f28d146f0ffdf46380e5
504 <504@zywx.com>
2021-11-21 13:49:49 +0800

    feat: 链表的练习

---
+++ 练习场/linked/single_linked_list.js
@@ -0,0 +1,65 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+class SingleLinkedList{
+    constructor(){
+        this.size=0;
+        this.head = new Node('head');
+        this.currentNode = '';
+    }
+
+    /**
+     * 
+     * 查找功能
+     * 1. findByContent
+     * 2. findByIndex
+     * 3. findIndexByContent
+     * 
+     * **/ 
+
+    // 1.通过元素的data查找元素
+    findByContent(){
+
+    }
+
+
+
+    // 2.通过index查找元素
+    findByIndex(){
+
+    }
+
+    // 3. 通过元素的data查找index    跟findByContent很像
+    findIndexByContent(){
+
+    }
+
+
+
+    /**
+     * 
+     * 插入功能
+     * 1. 
+     * 
+     * 
+     * **/
+
+    insert(){}
+    append(){}
+
+
+
+    remove(){}
+
+}
+
+
+
+
+
+
